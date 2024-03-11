7cbd5acaf4220f230988c29f9a50e515659f7196
0TKA3 <derakrun@gmail.com>
2024-03-11 16:25:42 +0300

    добавлены классы для текста

--- src/components/Home/NewGames/index.tsx
+++ src/components/Home/NewGames/index.tsx
@@ -5,7 +5,7 @@ export default function NewGames() {
         <div className="mt-7 py-6 px-4 custom-border flex flex-col">
             <div className="flex gap-3 items-center">
                 <img src={lightningIcon} alt="new" />
-                <h1 className="text-header">Новинки</h1>
+                <h1 className="text-subtitle">Новинки</h1>
             </div>
             <div></div>
         </div>
