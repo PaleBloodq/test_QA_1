0c20d4c05fcf30eb3a84cf24c4f7949c37b59f1d
0TKA3 <lal4topol@yandex.ru>
2024-04-28 10:29:34 +0300

    Добавлена страницу отсутсвия аккаунта

--- frontend/src/pages/Profile/index.tsx
+++ frontend/src/pages/Profile/index.tsx
@@ -8,12 +8,15 @@ import { OrderType } from "../../types/orderType";
 import { useDispatch, useSelector } from "react-redux";
 import { setUserData, setUserName, updateAccountMail, updateAccountPassword, updateBillMail } from "../../features/User/userSlice";
 import { userSelector } from "../../features/User/userSelectors";
+import Navigation from "../../components/common/Navigation";
+import { useNavigate } from "react-router";
 
 export default function Profile() {
     const dispatch = useDispatch()
+    const navigate = useNavigate()
     const { data: ordersData = [], isLoading: isOrdersLoading } = useGetOrdersQuery({})
     const [showOrderHistory, setShowOrderHistory] = useState(false)
-    const { userData, updatedData, username } = useSelector(userSelector)
+    const { userData, updatedData, username, isLoggined } = useSelector(userSelector)
     const [updateUserData, { error, status }] = useUpdateUserDataMutation();
 
     function handleUpdateUserData() {
@@ -39,67 +42,75 @@ export default function Profile() {
 
     return (
         <Container>
-            <div className="w-full h-[100px] custom-border px-6 flex items-center justify-start">
-                {!isOrdersLoading &&
-                    <>
-                        <div className="w-14 h-14 rounded-full bg-blue-400 mr-6"></div>
-                        <div className="flex flex-col gap-[10px]">
-                            <h1 className="text-title-xl">{username}</h1>
-                            <div className="flex gap-4">
-                                <p className="text-subtitle">Баллы:</p>
-                                <Tag type="discount">{userData?.cashback} ₽</Tag>
-                            </div>
-                        </div>
-                    </>
-                }
-            </div>
-            <div className="w-full flex gap-3 mt-6">
-                <button onClick={() => setShowOrderHistory(false)} className={`w - full h - [33px] text - [14px] ${!showOrderHistory ? "rounded-lg red-gradient font-bold text-white" : "rounded-lg border dark:border-[#FFFFFF1A] text-[#606D7B] dark:text-[#FFFFFF99]"} `} >
-                    Данные покупателя
-                </button>
-                <button onClick={() => setShowOrderHistory(true)} className={`w - full h - [33px] text - [14px] ${showOrderHistory ? "rounded-lg red-gradient font-bold text-white" : "rounded-lg border dark:border-[#FFFFFF1A] text-[#606D7B] dark:text-[#FFFFFF99]"} `} >
-                    История заказов
-                </button>
-            </div>
-            {!showOrderHistory
-                ?
-                <div className="w-full flex flex-col mt-7">
-                    <p className="text-subtitle mb-3">E-mail от аккаунта PlayStation:</p>
-                    <Input localValue={false} hardlyEditable={true} placeholder="E-Mail" type="email" value={updatedData.psEmail} setValue={updateAccountMail} />
-                    <p className="text-subtitle mb-3">Пароль от аккаунта PlayStation:</p>
-                    <Input localValue={false} hardlyEditable={true} placeholder="Пароль" type="password" value={updatedData.psPassword} setValue={updateAccountPassword} />
-                    <p className="text-subtitle mb-3">E-mail для чеков:</p>
-                    <Input localValue={false} hardlyEditable={true} placeholder="E-Mail" type="email" value={updatedData.billEmail} setValue={updateBillMail} />
-                    <span className="mt-36">
-                        <Button onClick={handleUpdateUserData}>Сохранить изменения</Button>
-                    </span>
-                </div>
-                :
-                <div className="w-full flex flex-col mt-7 gap-[13px]">
-                    {!isOrdersLoading && ordersData?.map((order: OrderType, index: number) =>
-                        <div key={index} className="w-full custom-border p-5 flex flex-col">
-                            <div className="flex w-full justify-between mb-6">
-                                <h1 className="text-title">Заказ от {order.date}</h1>
-                                <div className="flex gap-2 items-center">
-                                    <h2 className="text-title">{order.amount} ₽</h2>
-                                    {orderStatusIcon(order.status)}
+            {isLoggined ? <>
+                <div className="w-full h-[100px] custom-border px-6 flex items-center justify-start">
+                    {!isOrdersLoading &&
+                        <>
+                            <div className="w-14 h-14 rounded-full bg-blue-400 mr-6"></div>
+                            <div className="flex flex-col gap-[10px]">
+                                <h1 className="text-title-xl">{username}</h1>
+                                <div className="flex gap-4">
+                                    <p className="text-subtitle">Баллы:</p>
+                                    <Tag type="discount">{userData?.cashback} ₽</Tag>
                                 </div>
                             </div>
-                            <div className="w-full flex flex-col">
-                                {order.order_products.map((cartItem, index: number) => <React.Fragment key={index} >
-                                    <div className="flex flex-col gap-1">
-                                        <h1 className="text-title-sm">{cartItem.item}</h1>
-                                        <p className="text-subtitle">{cartItem.description}</p>
-                                        <h2 className="text-title">{cartItem.price} ₽</h2>
-                                    </div>
-                                    {index !== order.order_products.length - 1 && <div className="w-full h-[1px] bg-[#E7E7E8] dark:bg-[#FFFFFF0D] my-6"></div>}
-                                </React.Fragment>
-                                )}
-                            </div>
-                        </div>
-                    )}
+                        </>
+                    }
                 </div>
-            }
+                <div className="w-full flex gap-3 mt-6">
+                    <button onClick={() => setShowOrderHistory(false)} className={`w - full h - [33px] text - [14px] ${!showOrderHistory ? "rounded-lg red-gradient font-bold text-white" : "rounded-lg border dark:border-[#FFFFFF1A] text-[#606D7B] dark:text-[#FFFFFF99]"} `} >
+                        Данные покупателя
+                    </button>
+                    <button onClick={() => setShowOrderHistory(true)} className={`w - full h - [33px] text - [14px] ${showOrderHistory ? "rounded-lg red-gradient font-bold text-white" : "rounded-lg border dark:border-[#FFFFFF1A] text-[#606D7B] dark:text-[#FFFFFF99]"} `} >
+                        История заказов
+                    </button>
+                </div>
+                {!showOrderHistory
+                    ?
+                    <div className="w-full flex flex-col mt-7">
+                        <p className="text-subtitle mb-3">E-mail от аккаунта PlayStation:</p>
+                        <Input localValue={false} hardlyEditable={true} placeholder="E-Mail" type="email" value={updatedData.psEmail} setValue={updateAccountMail} />
+                        <p className="text-subtitle mb-3">Пароль от аккаунта PlayStation:</p>
+                        <Input localValue={false} hardlyEditable={true} placeholder="Пароль" type="password" value={updatedData.psPassword} setValue={updateAccountPassword} />
+                        <p className="text-subtitle mb-3">E-mail для чеков:</p>
+                        <Input localValue={false} hardlyEditable={true} placeholder="E-Mail" type="email" value={updatedData.billEmail} setValue={updateBillMail} />
+                        <span className="mt-36">
+                            <Button onClick={handleUpdateUserData}>Сохранить изменения</Button>
+                        </span>
+                    </div>
+                    :
+                    <div className="w-full flex flex-col mt-7 gap-[13px]">
+                        {!isOrdersLoading && ordersData?.map((order: OrderType, index: number) =>
+                            <div key={index} className="w-full custom-border p-5 flex flex-col">
+                                <div className="flex w-full justify-between mb-6">
+                                    <h1 className="text-title">Заказ от {order.date}</h1>
+                                    <div className="flex gap-2 items-center">
+                                        <h2 className="text-title">{order.amount} ₽</h2>
+                                        {orderStatusIcon(order.status)}
+                                    </div>
+                                </div>
+                                <div className="w-full flex flex-col">
+                                    {order.order_products.map((cartItem, index: number) => <React.Fragment key={index} >
+                                        <div className="flex flex-col gap-1">
+                                            <h1 className="text-title-sm">{cartItem.item}</h1>
+                                            <p className="text-subtitle">{cartItem.description}</p>
+                                            <h2 className="text-title">{cartItem.price} ₽</h2>
+                                        </div>
+                                        {index !== order.order_products.length - 1 && <div className="w-full h-[1px] bg-[#E7E7E8] dark:bg-[#FFFFFF0D] my-6"></div>}
+                                    </React.Fragment>
+                                    )}
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                }
+            </> : <>
+                <Navigation />
+                <div className="flex flex-col gap-7 my-52">
+                    <h1 className="text-header text-center">Профиль появится автоматически после вашего первого заказа</h1>
+                    <Button onClick={() => navigate(-1)}>Вернуться к каталогу</Button>
+                </div>
+            </>}
         </Container>
     )
 }
