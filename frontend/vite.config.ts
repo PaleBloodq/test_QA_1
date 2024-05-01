18aeb2920f7e3ee218f318cbda602b36e71bf8cf
1 <nervcop@gmail.com>
2024-05-02 01:57:04 +0300

    али за тебя

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/aoki-store",
+  base: import.meta.env.VITE_BASE_URL,
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
