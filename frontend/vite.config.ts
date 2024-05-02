1defc53224f171b732773c3b36dc2d64f78de6f5
1 <nervcop@gmail.com>
2024-05-02 04:45:36 +0300

    али за тебя и за себя за всех нас

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,37 @@
-import { defineConfig } from "vite";
+import {defineConfig, loadEnv} from 'vite'
 import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: import.meta.env.VITE_BASE_URL,
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@icons": path.resolve(__dirname, "./src/assets/icons"),
-      "@services": path.resolve(__dirname, "./src/services"),
-      "@components": path.resolve(__dirname, "./src/components/"),
-    },
-  },
-});
+export default ({mode}) => {
+    process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+    return defineConfig({
+        plugins: [react()],
+        base: process.env.VITE_BASE_URL,
+        esbuild: {
+            keepNames: true,
+        },
+        build: {
+            outDir: 'dist',
+            rollupOptions: {
+                output: {
+                    manualChunks: {
+                        vendor: ['react', 'react-dom']
+                    }
+                }
+            },
+            minify: 'terser',
+            optimizeDeps: {
+                include: ['react', 'react-dom']
+            }
+        },
+        resolve: {
+            alias: {
+                "@": path.resolve(__dirname, "./src"),
+                "@icons": path.resolve(__dirname, "./src/assets/icons"),
+                "@services": path.resolve(__dirname, "./src/services"),
+                "@components": path.resolve(__dirname, "./src/components/"),
+            },
+        },
+    });
+}
