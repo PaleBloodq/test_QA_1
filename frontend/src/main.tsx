7e8014a7ba611c2ca7c75c37a4140aa4d85756d6
0TKA3 <lal4topol@yandex.ru>
2024-05-03 15:02:16 +0300

    вернул старый роутер, добавил куки

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,13 +2,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './assets/styles/global.css'
 import { Provider } from 'react-redux'
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { store } from './store/store.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    <HashRouter basename={'/'}>
+    <BrowserRouter basename={import.meta.env.VITE_BASE_URL}>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </Provider>
 )
--- frontend/src/services/userApi.ts
+++ frontend/src/services/userApi.ts
@@ -1,11 +1,12 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import cookie from 'cookiejs'
 
 export const userApi = createApi({
   reducerPath: "userApi",
   baseQuery: fetchBaseQuery({
     baseUrl: import.meta.env.VITE_API_URL,
     headers: {
-      Authorization: `Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZWxlZ3JhbV9pZCI6MjM0MjMsInRva2VuX2V4cGlyYXRvbiI6IjIwMjQtMDUtMDJUMjI6Mjc6MTcuOTU3NDQ4IiwidG9rZW5fc2VlZCI6IjllZDJiOWI3YzVkMzQxODVhZGMxNTU4ODhkNTJmMzJmIn0.vt0pZJUwNCLvQs8DG5FLPWdjynMX0P7aXkoHo5JGxSgVlNd5Q0qkby0X986-4gnd2zrNzBHGSYzdxdpsnIj880qZSoHCtixaNH32JosQopjW4LJugtvKQMkxrbbQDwU-9wjU1ptDp_ezFAWDa_U53QcRtbweAkQDfG_TRkYfH0RPOHn9eJ8b8vdTYYyXC5LGLIAQYRFDrAToI3gCvcoAs1vjTJ7bgOPuobgYKWWEM1vy0Qf2_I7OJekwompIi0AlgLsbfhQNKGkPAwvIkXihCKglBM9XGRBiThHjEyySUcu_ig05snUWO7-N6RGaZMxbHnm_ynQqyzBHIYz5yl40-Q`,
+      Authorization: `Bearer ${cookie.get('token')}`,
     },
   }),
   endpoints: (builder) => ({
