193f2d569a9c2043c5201a94b65c517b5fbdb818
RostHarcha <rostiki.com@mail.ru>
2024-05-04 20:07:38 +0300

    Переработал создание заказов

--- backend/api/views/orders.py
+++ backend/api/views/orders.py
@@ -1,11 +1,13 @@
 import os
 from datetime import date
+from dataclasses import dataclass
 import requests
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from django.db.models import Sum
+from django.db.models.manager import BaseManager
 from api import models, serializers, utils
 
 PAYMENTS_URL = f'{os.environ.get("PAYMENTS_SCHEMA")}://{os.environ.get("PAYMENTS_HOST")}'
@@ -40,72 +42,124 @@ class CheckPromoCode(APIView):
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
-class CreateOrder(APIView):
+@dataclass
+class OrderInfo:
+    profile: models.Profile
+    cart: BaseManager[models.ProductPublication]
+    spend_cashback: bool
+    need_account: bool
+    bill_email: str
+    remember_account: bool
+    email: str
+    password: str
+    promo_code: str
+    
+    def get_promo_code_discount(self) -> int | None:
+        discount = utils.check_promo_code(self.promo_code)
+        if not discount:
+            self.promo_code = None
+            discount = 0
+        return discount
+    
+    def get_amount(self, promo_code_discount: int = None) -> int:
+        amount = self.cart.aggregate(Sum('final_price')).get('final_price__sum')
+        if promo_code_discount:
+            amount -= amount * promo_code_discount / 100
+        if self.spend_cashback:
+            if self.profile.cashback >= amount:
+                return 1
+            amount -= self.profile.cashback
+        return amount
+    
     def get_description(self, publication: models.ProductPublication) -> str:
         description = f'{publication.title}'
         for platform in publication.platforms.all():
             description += f' - {platform.name}'
         return description
     
+    def remember_ps_account(self):
+        self.profile.playstation_email = self.email
+        self.profile.playstation_password = self.password
+        self.profile.bill_email = self.bill_email
+        self.profile.save()
+    
+    def fill_order(self, order: models.Order, promo_code_discount: int) -> models.Order:
+        if self.remember_account:
+            self.remember_ps_account()
+        order.cashback = 0
+        for publication in self.cart:
+            if not self.spend_cashback:
+                order.cashback += publication.final_price * publication.cashback / 100
+            models.OrderProduct.objects.get_or_create(
+                order=order,
+                product=publication.product.title,
+                product_id=publication.id,
+                description=self.get_description(publication),
+                original_price=publication.original_price,
+                final_price=publication.final_price,
+            )
+        if order.promo_code_discount:
+            order.cashback -= order.cashback * promo_code_discount / 100
+        payment = serializers.PaymentSerializer(
+            data=requests.post(f'{PAYMENTS_URL}/create_payment', json={
+                'order_id': str(order.id),
+                'amount': order.amount,
+                'description': str(order),
+                'customer_telegram_id': order.profile.telegram_id,
+            }).json()
+        )
+        if payment.is_valid():
+            order.payment_id = payment.validated_data.get('payment_id')
+            order.payment_url = payment.validated_data.get('payment_url')
+        order.save()
+        return order
+    
+    def create_order(self) -> models.Order:
+        if not self.need_account:
+            if not self.email:
+                self.email = self.profile.playstation_email
+            if not self.password:
+                self.password = self.profile.playstation_password
+        promo_code_discount = self.get_promo_code_discount()
+        order, created = models.Order.objects.get_or_create(
+            profile=self.profile,
+            date=date.today(),
+            amount=self.get_amount(promo_code_discount),
+            need_account=self.need_account,
+            email=self.email,
+            password=self.password,
+            bill_email=self.bill_email,
+            spend_cashback=self.spend_cashback,
+            status=models.Order.StatusChoices.CREATED,
+            promo_code=self.promo_code,
+            promo_code_discount=promo_code_discount,
+        )
+        if created:
+            order = self.fill_order(order, promo_code_discount)
+        return order
+
+
+class CreateOrder(APIView):
     @utils.auth_required
     def post(self, request: Request, profile: models.Profile):
-        cart = models.ProductPublication.objects.filter(
-            id__in=request.data.get('cart', [])
+        order_info = OrderInfo(
+            profile,
+            models.ProductPublication.objects.filter(id__in=request.data.get('cart', [])),
+            request.data.get('spendCashback', False),
+            not request.data.get('hasAccount', False),
+            request.data.get('billEmail', profile.bill_email),
+            request.data.get('rememberAccount', False),
+            request.data.get('accountEmail'),
+            request.data.get('accountPassword'),
+            request.data.get('promoCode'),
         )
-        spend_cashback: bool = request.data.get('spendCashback', False)
-        email = request.data.get('accountEmail', profile.playstation_email)
-        password = request.data.get('accountPassword', profile.playstation_password)
-        bill_email = request.data.get('billEmail', profile.bill_email)
-        promo_code = request.data.get('promoCode')
-        promo_code_discount = utils.check_promo_code(promo_code)
-        remember_account: bool = request.data.get('rememberAccount', False)
-        if cart and email and password and bill_email:
-            amount = cart.aggregate(Sum('final_price')).get('final_price__sum')
-            if promo_code_discount:
-                amount -= amount * promo_code_discount / 100
-            order, order_created = models.Order.objects.get_or_create(
-                profile=profile,
-                date=date.today(),
-                amount=amount,
-                email=email,
-                password=password,
-                bill_email=bill_email,
-                spend_cashback=spend_cashback,
-                status=models.Order.StatusChoices.CREATED,
-                promo_code=promo_code,
-                promo_code_discount=promo_code_discount,
-            )
-            if order_created:
-                if remember_account:
-                    profile.playstation_email = email
-                    profile.playstation_password = password
-                    profile.bill_email = bill_email
-                    profile.save()
-                order.cashback = 0
-                for publication in cart:
-                    order.cashback += publication.final_price * publication.cashback / 100
-                    models.OrderProduct.objects.get_or_create(
-                        order=order,
-                        product=publication.product.title,
-                        product_id=publication.id,
-                        description=self.get_description(publication),
-                        original_price=publication.original_price,
-                        final_price=publication.final_price,
-                    )
-                if order.promo_code_discount:
-                    order.cashback -= order.cashback * promo_code_discount / 100
-                payment = serializers.PaymentSerializer(
-                    data=requests.post(f'{PAYMENTS_URL}/create_payment', json={
-                        'order_id': str(order.id),
-                        'amount': order.amount,
-                        'description': str(order),
-                        'customer_telegram_id': profile.telegram_id,
-                    }).json()
-                )
-                if payment.is_valid():
-                    order.payment_id = payment.validated_data.get('payment_id')
-                    order.payment_url = payment.validated_data.get('payment_url')
-                order.save()
+        if not order_info.need_account:
+            if not order_info.email:
+                order_info.email = profile.playstation_email
+            if not order_info.password:
+                order_info.password = profile.playstation_password
+        if order_info.cart and order_info.bill_email:
+            order = order_info.create_order()
             return Response(
                 {
                     'PaymentUrl': order.payment_url
