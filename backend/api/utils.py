4abeddec11a60e55031cf674611a0ee9e2079ef9
RostHarcha <rostiki.com@mail.ru>
2024-05-05 19:11:35 +0300

    Поправил рефреш токена

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -10,6 +10,7 @@ from cryptography.hazmat.primitives import serialization
 import jwt
 from api import models
 
+
 with open(os.environ.get('RSA_PEM_FILE'), "rb") as key_file:
     private_key = serialization.load_pem_private_key(
         key_file.read(),
@@ -17,11 +18,13 @@ with open(os.environ.get('RSA_PEM_FILE'), "rb") as key_file:
         backend=default_backend()
     )
 
+
 with open(os.environ.get('RSA_PUB_FILE'), "rb") as key_file:
     public_key = serialization.load_pem_public_key(
         key_file.read(),
         backend=default_backend()
     )
+
 
 def get_ip(request: Request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
@@ -46,20 +49,17 @@ def encode_profile(telegram_id: int) -> str:
     }, private_key, 'RS256')
 
 
-def decode_token(token: str) -> models.Profile | None:
+def decode_token(token: str, check_profile_only: bool = False) -> models.Profile | None:
     try:
         decoded = jwt.decode(token, public_key, ['RS256'])
         telegram_id = decoded.get('telegram_id')
         token_expiraton = decoded.get('token_expiraton')
         token_seed = decoded.get('token_seed')
         if telegram_id and token_expiraton and token_seed:
-            token_expiraton = datetime.fromisoformat(token_expiraton)
             profile = models.Profile.get_or_none(telegram_id=telegram_id)
-            if (
-                profile
-                and token_seed == profile.token_seed.hex
-                and token_expiraton >= datetime.now()
-            ):
+            if check_profile_only:
+                return profile
+            if token_seed == profile.token_seed.hex and datetime.fromisoformat(token_expiraton) >= datetime.now():
                 return profile
     except:
         pass
