55d4a9086120043f78a25a55bbb19f4a284a11ee
0TKA3 <lal4topol@yandex.ru>
2024-05-05 20:46:44 +0300

    + фикс

--- frontend/src/helpers/replaceUrl.ts
+++ frontend/src/helpers/replaceUrl.ts
@@ -1,7 +1,7 @@
 import placeholderImg from "@images/placeholder.png";
 
 export function replaceUrl(url: string): string {
-  if (url !== null) {
+  if (url !== null && url !== undefined) {
     return `https://chatlabs.site/aokibot/backend/${url}`;
   } else {
     return placeholderImg;
