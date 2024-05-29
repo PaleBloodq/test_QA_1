18a956c6171aba5cb984c766cb21660cfad9a589
RostHarcha <rostiki.com@mail.ru>
2024-05-29 18:51:43 +0300

    Обернул парсинг в try

--- backend/api/tasks.py
+++ backend/api/tasks.py
@@ -21,44 +21,47 @@ def parse_product_publications_task(product_ids: list[str], need_notify=True):
         logging.warning(f'{len(editions)} editions found.')
         logging.warning(f'{editions=}')
         for edition in editions:
-            publication = models.ProductPublication.objects.filter(ps_store_id=edition.id).first()
-            if publication:
-                if not publication.parsing_enabled:
-                    async_to_sync(send_admin_notification)({
-                        'text': f'Цена на издание товара {publication.product.title} изменилась',
-                        'level': NotifyLevels.WARN.value,
-                    })
-                    publication.price_changed = False
-                    publication.save()
-                    return publication
-                if publication.final_price != edition.price.discounted_price:
-                    publication.price_changed = True
-                publication.final_price = utils.normalize_price(edition.price.discounted_price, True)
-                publication.discount = edition.price.discount
-                publication.discount_deadline = edition.price.discount_deadline
-                publication.ps_plus_final_price = utils.normalize_price(edition.ps_plus_price.discounted_price, True)
-                publication.ps_plus_discount = edition.ps_plus_price.discount
-                publication.ps_plus_discount_deadline = edition.ps_plus_price.discount_deadline
-            else:
-                publication = models.ProductPublication(
-                    product=product,
-                    title=edition.name,
-                    ps_store_id=edition.id,
-                    final_price=utils.normalize_price(edition.price.discounted_price, True),
-                    discount=edition.price.discount,
-                    discount_deadline=edition.price.discount_deadline,
-                    ps_plus_final_price=utils.normalize_price(edition.ps_plus_price.discounted_price, True),
-                    ps_plus_discount=edition.ps_plus_price.discount,
-                    ps_plus_discount_deadline=edition.ps_plus_price.discount_deadline,
-                )
-                if edition.release_date != product.release_date:
-                    product.release_date = edition.release_date
-                    product.save()
-            if not all((publication.photo, publication.preview)):
-                publication.set_photo_from_url(edition.image)
-            publication.save()
-            for platform in edition.platforms:
-                publication.platforms.add(models.Platform.objects.get_or_create(name=platform)[0])
+            try:
+                publication = models.ProductPublication.objects.filter(ps_store_id=edition.id).first()
+                if publication:
+                    if not publication.parsing_enabled:
+                        async_to_sync(send_admin_notification)({
+                            'text': f'Цена на издание товара {publication.product.title} изменилась',
+                            'level': NotifyLevels.WARN.value,
+                        })
+                        publication.price_changed = False
+                        publication.save()
+                        return publication
+                    if publication.final_price != edition.price.discounted_price:
+                        publication.price_changed = True
+                    publication.final_price = utils.normalize_price(edition.price.discounted_price, True)
+                    publication.discount = edition.price.discount
+                    publication.discount_deadline = edition.price.discount_deadline
+                    publication.ps_plus_final_price = utils.normalize_price(edition.ps_plus_price.discounted_price, True)
+                    publication.ps_plus_discount = edition.ps_plus_price.discount
+                    publication.ps_plus_discount_deadline = edition.ps_plus_price.discount_deadline
+                else:
+                    publication = models.ProductPublication(
+                        product=product,
+                        title=edition.name,
+                        ps_store_id=edition.id,
+                        final_price=utils.normalize_price(edition.price.discounted_price, True),
+                        discount=edition.price.discount,
+                        discount_deadline=edition.price.discount_deadline,
+                        ps_plus_final_price=utils.normalize_price(edition.ps_plus_price.discounted_price, True),
+                        ps_plus_discount=edition.ps_plus_price.discount,
+                        ps_plus_discount_deadline=edition.ps_plus_price.discount_deadline,
+                    )
+                    if edition.release_date != product.release_date:
+                        product.release_date = edition.release_date
+                        product.save()
+                if not all((publication.photo, publication.preview)):
+                    publication.set_photo_from_url(edition.image)
+                publication.save()
+                for platform in edition.platforms:
+                    publication.platforms.add(models.Platform.objects.get_or_create(name=platform)[0])
+            except Exception as exc:
+                logging.exception(exc)
     async_to_sync(send_admin_notification)({
         'text': f'Парсинг завершился',
         'level': NotifyLevels.WARN.value,
