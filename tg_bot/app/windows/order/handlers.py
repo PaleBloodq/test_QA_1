a9e61bca557a02653e042074a846cd321226d992
1 <nervcop@gmail.com>
2024-06-16 06:38:15 +0300

    Revert "че" пойдет

--- tg_bot/app/windows/order/handlers.py
+++ tg_bot/app/windows/order/handlers.py
@@ -21,14 +21,17 @@ async def answer_order(message: types.Message, dialog_manager: DialogManager | N
     regex = r"([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})"
     match = re.search(regex, callback)
     order_id = match.group(1)
+    image_bs64 = None
+    text = message.text or message.caption
+    if not text:
+        await message.reply('Ответ не может быть без текста')
+        return
     if order_id:
         if message.photo:
             image = message.photo[-1].file_id
             image_io = io.BytesIO()
-            bot.download(image, destination=image_io)
+            await bot.download(image, destination=image_io)
             image_bs64 = base64.b64encode(image_io.getvalue()).decode('utf-8')
-            await bootstrap.ApiWrapper.send_message(order_id=order_id, text=message.text, image=image_bs64)
-        base64.b64encode(order_id)
-        await bootstrap.ApiWrapper.send_message(order_id=order_id, text=message.text)
+        await bootstrap.ApiWrapper.send_message(order_id=order_id, text=text, image=image_bs64)
         print(order_id)
         await message.reply('Отлично! Ответ отправлен')
