3544fc97080691df70e80a2f1f33070e04d5b2d2
1 <nervcop@gmail.com>
2024-06-16 05:58:43 +0300

    Revert "че"
    This reverts commit e21c214c

--- tg_bot/app/windows/order/handlers.py
+++ tg_bot/app/windows/order/handlers.py
@@ -1,3 +1,5 @@
+import base64
+import io
 import re
 
 from aiogram import types
@@ -6,7 +8,7 @@ from aiogram_dialog import DialogManager
 import bootstrap
 from states.states import MainSG
 
-
+bot = bootstrap.MyBot().getInstance()
 async def start(message: types.Message, dialog_manager: DialogManager | None = None):
     await dialog_manager.start(state=MainSG.main_menu)
 
@@ -20,6 +22,13 @@ async def answer_order(message: types.Message, dialog_manager: DialogManager | N
     match = re.search(regex, callback)
     order_id = match.group(1)
     if order_id:
+        if message.photo:
+            image = message.photo[-1].file_id
+            image_io = io.BytesIO()
+            bot.download(image, destination=image_io)
+            image_bs64 = base64.b64encode(image_io.getvalue()).decode('utf-8')
+            await bootstrap.ApiWrapper.send_message(order_id=order_id, text=message.text, image=image_bs64)
+        base64.b64encode(order_id)
         await bootstrap.ApiWrapper.send_message(order_id=order_id, text=message.text)
         print(order_id)
         await message.reply('Отлично! Ответ отправлен')
--- tg_bot/web/api/models.py
+++ tg_bot/web/api/models.py
@@ -12,13 +12,16 @@ class OrderExtra:
     status_text: str
     emoji: str
     text: str
+
     def __post_init__(self):
         self.text = utils.escape_markdown(self.text)
+
 
 class NewMessage(BaseModel):
     order_id: str
     text: str
     user_id: int
+    images: Optional[List[str]]
 
 
 class OrderProduct(BaseModel):
@@ -36,11 +39,13 @@ class Order(BaseModel):
     payment_url: Optional[str]
     need_account: bool
     status: Literal['IN_PROGRESS', 'COMPLETED', 'ERROR', 'PAID', 'PAYMENT']
+
     @field_validator('order_id', 'date')
     def process_text_fields(cls, value):
         if isinstance(value, str):
             return utils.escape_markdown(value)
         return value
+
     def get_order_extra(self) -> Optional[OrderExtra]:
         match self.status:
             case 'IN_PROGRESS':
